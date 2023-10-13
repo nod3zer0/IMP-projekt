c1fba98849f625ac714dd78c3f86695493c89ffa
nod3zer0 <xceska06@stud.fit.vutbr.cz>
2023-10-13 22:24:42 +0200

    timestamps

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -13,6 +13,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// sntp
+#include "esp_netif_sntp.h"
+#include "esp_sntp.h"
+
+// timer
+#include "driver/gpio.h"
+#include "esp_timer.h"
+
 // wifi
 #include "esp_event.h"
 #include "esp_wifi.h"
@@ -31,6 +39,10 @@ static const char *TAG = "ADC EXAMPLE";
 int temp_limit = 0;
 int hystereze = 0;
 int led_is_on = 0;
+
+nvs_handle_t nvsHandle;
+nvs_handle_t nvsTemperature;
+esp_err_t err;
 
 static esp_adc_cal_characteristics_t adc1_chars;
 
@@ -124,7 +136,7 @@ void wifi_init_sta(void) {
   vEventGroupDelete(s_wifi_event_group);
 }
 
-nvs_handle_t init_nvs(esp_err_t *err) {
+void init_nvs(esp_err_t *err) {
 
   *err = nvs_flash_init();
   if (*err == ESP_ERR_NVS_NO_FREE_PAGES ||
@@ -135,10 +147,6 @@ nvs_handle_t init_nvs(esp_err_t *err) {
     *err = nvs_flash_init();
   }
   ESP_ERROR_CHECK(*err);
-
-  nvs_handle_t nvsHandle;
-  *err = nvs_open("storage", NVS_READWRITE, &nvsHandle);
-  return nvsHandle;
 }
 
 int write_temp_to_nvs(nvs_handle_t nvsHandle, double temp) {
@@ -211,26 +219,153 @@ esp_err_t root_post_handler(httpd_req_t *req) {
   return ESP_OK;
 }
 
+esp_err_t getTempsHandler(httpd_req_t *req) {
+  // get all timestamps from nvs
+  // get all temps from nvs
+
+  // Example of listing all the key-value pairs of any type under specified
+  // handle (which defines a partition and namespace)
+  char *list = malloc(1000);
+  nvs_iterator_t it = NULL;
+  esp_err_t res = nvs_entry_find("nvs", "temp", NVS_TYPE_ANY, &it);
+  sprintf(list, "<h1> Temperatures </h1>\n");
+  sprintf(list + strlen(list),
+          "<table> <tr> <th> Time </th> <th> Temp </th> </tr>");
+
+  // print res error name
+  printf("Error: %s\n", esp_err_to_name(res));
+
+  while (res == ESP_OK) {
+    nvs_entry_info_t info;
+    nvs_entry_info(it, &info);
+
+    char time[300];
+    sprintf(time, "%s", info.key);
+    // convert to  human readable time
+    time_t t = atoi(time);
+    struct tm tm;
+    localtime_r(&t, &tm);
+    char timeHuman[300];
+    strftime(timeHuman, sizeof(timeHuman), "%c", &tm);
+
+    // read value
+
+    int32_t temp_int = 0;
+    nvs_get_i32(nvsTemperature, info.key, &temp_int);
+
+    sprintf(list + strlen(list), "<tr><td> %s </td>\n<td> %f </td>\n</tr>\n",
+            timeHuman, (temp_int / 1000.0));
+    char *tmpList = realloc(list, strlen(list) + 200);
+    if (tmpList != NULL) {
+      list = tmpList;
+    }
+    res = nvs_entry_next(&it);
+  }
+  nvs_release_iterator(it);
+  sprintf(list + strlen(list), "</table>");
+
+  httpd_resp_send(req, list, HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
+}
+
+void timeInit() {
+
+  esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
+  esp_netif_sntp_init(&config);
+  if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
+    printf("Failed to update system time within 10s timeout");
+  }
+  // set GMT+2 time zone
+  setenv("TZ", "GMT+2", 1);
+  tzset();
+}
+
+char *getTimeNow() {
+  time_t now;
+  struct tm timeinfo;
+  time(&now);
+  localtime_r(&now, &timeinfo);
+  char *timeNow = malloc(100);
+  strftime(timeNow, 100, "%x-%X", &timeinfo);
+  return timeNow;
+}
+
+int saveTempToNVSwithTimestamp(nvs_handle_t nvsHandle, double temp) {
+  esp_err_t err;
+  char timeNow[100];
+  int32_t temp_int = temp * 1000;
+  time_t now;
+  time(&now);
+  sprintf(timeNow, "%llu", now);
+  err = nvs_set_i32(nvsHandle, timeNow, temp_int);
+  nvs_commit(nvsHandle);
+  return err;
+}
+
+void readTemperature_timer(void *param) {
+  uint32_t voltage;
+  voltage =
+      esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_7), &adc1_chars);
+  double temp = ((8.194 - (sqrt(((-8.194) * (-8.194)) +
+                                (4 * 0.00262 * (1324.0 - voltage * 1.0))))) /
+                 (2 * (-0.00262))) +
+                30;
+  ESP_LOGI(TAG, "ADC1_CHANNEL_6: %d mV", (int)voltage);
+  ESP_LOGI(TAG, "Temp: %f C", temp);
+  write_temp_to_nvs(nvsHandle, temp);
+  saveTempToNVSwithTimestamp(nvsTemperature, temp);
+  double temp2 = 0;
+  temp2 = read_temp_from_nvs(nvsHandle, &err);
+  ESP_LOGI(TAG, "Temp from NVS: %f C", temp2);
+  // ets_delay_us(1000000);
+
+  if ((temp < (temp_limit - hystereze)) && led_is_on == 1) {
+    led_is_on = 0;
+    gpio_set_level(LED_GPIO, 1);
+  }
+
+  if ((temp > (temp_limit + hystereze)) && led_is_on == 0) {
+    led_is_on = 1;
+    gpio_set_level(LED_GPIO, 0);
+  }
+  ESP_LOGI(TAG, "LED: %d", led_is_on);
+}
+
+void initTimer() {
+  const esp_timer_create_args_t my_timer_args = {
+      .callback = &readTemperature_timer, .name = "Read Temp"};
+  esp_timer_handle_t timer_handler;
+  ESP_ERROR_CHECK(esp_timer_create(&my_timer_args, &timer_handler));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(timer_handler, 1000000));
+}
+
 void app_main(void) {
 
   gpio_reset_pin(LED_GPIO);
-  /* Set the GPIO as a push/pull output */
+  // Set the GPIO as a push/pull output
   gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+  // init time
+  timeInit();
   // Initialize NVS
-  nvs_handle_t nvsHandle;
-  esp_err_t err;
-  nvsHandle = init_nvs(&err);
+
+  init_nvs(&err);
+  err = nvs_open("temp", NVS_READWRITE, &nvsTemperature);
   if (err != ESP_OK) {
     printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
   }
-
-  uint32_t voltage;
+  err = nvs_open("storage", NVS_READWRITE, &nvsHandle);
+  if (err != ESP_OK) {
+    printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+  }
 
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT,
                            0, &adc1_chars);
 
   ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
   ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11));
+
+  // wifi connect
   wifi_init_sta();
 
   // HTTP Server
@@ -249,34 +384,18 @@ void app_main(void) {
                         .handler = root_post_handler,
                         .user_ctx = NULL};
     httpd_register_uri_handler(server, &uri2);
+
+    httpd_uri_t getTempsUri = {.uri = "/getTemps",
+                               .method = HTTP_GET,
+                               .handler = getTempsHandler,
+                               .user_ctx = NULL};
+    httpd_register_uri_handler(server, &getTempsUri);
   }
+  // init timer
+  initTimer();
   //______________
   while (1) {
-    voltage =
-        esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_7), &adc1_chars);
-    double temp = ((8.194 - (sqrt(((-8.194) * (-8.194)) +
-                                  (4 * 0.00262 * (1324.0 - voltage * 1.0))))) /
-                   (2 * (-0.00262))) +
-                  30;
-    ESP_LOGI(TAG, "ADC1_CHANNEL_6: %d mV", (int)voltage);
-    ESP_LOGI(TAG, "Temp: %f C", temp);
-    write_temp_to_nvs(nvsHandle, temp);
-    double temp2 = 0;
-    temp2 = read_temp_from_nvs(nvsHandle, &err);
-    ESP_LOGI(TAG, "Temp from NVS: %f C", temp2);
-    ets_delay_us(1000000);
 
-    if ((temp < (temp_limit - hystereze))&& led_is_on == 1) {
-      led_is_on = 0;
-      gpio_set_level(LED_GPIO, 1);
-    }
-
-    if ((temp > (temp_limit + hystereze)) && led_is_on == 0) {
-      led_is_on = 1;
-      gpio_set_level(LED_GPIO, 0);
-    }
-    ESP_LOGI(TAG, "LED: %d", led_is_on);
-
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(100)); // to prevent watchdog reset
   }
 }
